d8fdbb2c6b9a0d7185ff780de170c50c7081db98
craypsy <edouardducrocq@hotmail.com>
2017-06-01 16:30:46 +0200

    Add files via upload

---
+++ app/controllers/display.php
@@ -0,0 +1,26 @@
+<?php
+namespace controllers;
+use libraries\Auth;
+use micro\orm\DAO;
+use Ajax\semantic\html\content\view\HtmlItem;
+
+
+/**
+ * Controller Display
+ **/
+class Display extends ControllerBase {
+
+    public function index(){
+
+
+
+    }
+    public function host($idhost){
+        $user = Auth::getUser();
+
+
+
+
+    }
+
+}
---
+++ app/controllers/my.php
@@ -0,0 +1,42 @@
+<?php
+namespace controllers;
+ use libraries\Auth;
+use micro\orm\DAO;
+use Ajax\semantic\html\content\view\HtmlItem;
+use models\Virtualhost;
+use models\Server;
+ 
+ /**
+ * Controller My
+ **/
+class My extends ControllerBase{
+	
+ 
+    /**
+     * Mes services
+     * Hosts et virtualhosts de l'utilisateur connect�
+     */
+    public function index(){
+        if(Auth::isAuth()){
+        	$user = Auth::getUser();
+            $hosts=DAO::getAll("models\Host","idUser=".$user->getId());
+            $virtualhosts=DAO::getAll("models\Virtualhost", "idServer!=".$user->getId());
+
+            $this->loadView("My/index.html",array("hosts"=>$hosts, "virtualhosts"=>$virtualhosts));
+
+
+        }
+        else {
+            Auth::getInfoUser($this);
+            $message=$this->semantic->htmlMessage("error","Merci de vous connecter pour tester.");
+            $message->setIcon("announcement")->setError();
+            $message->setDismissable();
+            $message->addContent(Auth::getInfoUser($this,"-login"));
+            echo $message;
+            echo $this->jquery->compile($this->view);
+
+        }
+
+    }
+   
+}

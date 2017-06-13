ccef8d8171d7880ddc8b3ccbef8ccf7eaa7af76c
root <root@debian>
2017-06-13 20:43:51 +0200

    x#

--- app/controllers/display.php
+++ app/controllers/display.php
@@ -6,36 +6,45 @@ use Ajax\semantic\html\content\view\HtmlItem;
 
 
 /**
- * Controller Display
- **/
+* Controller Display
+**/
 class Display extends ControllerBase {
 
-    public function index(){
+  public function index(){
 
 
 
-    }
-    public function host($idhost){
-        $user = Auth::getUser();
-        $hosts=DAO::getAll("models\Host","idUser=".$user->getId());
-        $servers=DAO::getAll("models\Server","idhost=".$idhost);
-        $virtualhosts=DAO::getAll("models\Virtualhost", "idServer=".$user->getId());
+  }
+  public function host($idhost){
+      if(Auth::isAuth()){        /** Si l'utilisateur est connecté */
+          $user = Auth::getUser();      /**  Stocke les informations de l'utilisateur connecté dans la variable user */
+          $hosts = DAO::getAll("models\Host", "idUser=" . $user->getId());      /** Récupère les données du model Host correspondant a l'Id */
+          $servers = DAO::getAll("models\Server", "idhost=" . $idhost);     /** Récupère les données du model Server correspondant a l'Idhost */
+          $virtualhosts = DAO::getAll("models\Virtualhost", "idServer=" . $user->getId());      /** Récupère les données du model Virtualhost correspondant a l'ddServer */
+
+                 /** Envoi les differents tableaux de données dans la vue index.html du dossier My */
+          $this->loadView("Display/host.html", array("hosts" => $hosts, "servers" => $servers, "virtualhosts" => $virtualhosts));
 
 
-        $this->loadView("Display/host.html",array("hosts"=>$hosts, "servers"=>$servers, "virtualhosts"=>$virtualhosts));
+      } else {
+          Auth::getInfoUser($this);
+          $message = $this->semantic->htmlMessage("error", "Merci de vous connecter pour tester.");
+          $message->setIcon("announcement")->setError();
+          $message->setDismissable();
+          $message->addContent(Auth::getInfoUser($this, "-login"));
+          echo $message;
+          echo $this->jquery->compile($this->view);
+
+      }
+  }
 
 
+  public function virtualhost($idvirtualhost){
+    $user = Auth::getUser();
+    $hosts=DAO::getAll("models\Host","idUser=".$user->getId());
+
+    $this->loadView("Display/virtualhost.html",array("hosts"=>$hosts));
 
 
-    }
-
-
-    public function virtualhost($idvirtualhost){
-        $user = Auth::getUser();
-        $hosts=DAO::getAll("models\Host","idUser=".$user->getId());
-
-        $this->loadView("Display/virtualhost.html",array("hosts"=>$hosts));
-
-
-    }
+  }
 }
--- app/controllers/my.php
+++ app/controllers/my.php
@@ -1,42 +1,41 @@
 <?php
 namespace controllers;
- use libraries\Auth;
+use libraries\Auth;
 use micro\orm\DAO;
 use Ajax\semantic\html\content\view\HtmlItem;
 use models\Virtualhost;
 use models\Server;
- 
- /**
- * Controller My
- **/
+
+/**
+* Controller My
+**/
 class My extends ControllerBase{
-	
- 
-    /**
-     * Mes services
-     * Hosts et virtualhosts de l'utilisateur connect�
-     */
-    public function index(){
-        if(Auth::isAuth()){
-        	$user = Auth::getUser();
-            $hosts=DAO::getAll("models\Host","idUser=".$user->getId());
-            $virtualhosts=DAO::getAll("models\Virtualhost", "idServer!=".$user->getId());
-
-            $this->loadView("My/index.html",array("hosts"=>$hosts, "virtualhosts"=>$virtualhosts));
 
 
-        }
-        else {
-            Auth::getInfoUser($this);
-            $message=$this->semantic->htmlMessage("error","Merci de vous connecter pour tester.");
-            $message->setIcon("announcement")->setError();
-            $message->setDismissable();
-            $message->addContent(Auth::getInfoUser($this,"-login"));
-            echo $message;
-            echo $this->jquery->compile($this->view);
-           
-        }
+  /**
+  * Mes services
+  * Hosts et virtualhosts de l'utilisateur connect�
+  */
+  public function index(){
+    if(Auth::isAuth()){          /** Si l'utilisateur est connecté */
+    $user = Auth::getUser();     /**  Stocke les informations de l'utilisateur connecté dans la variable user */
+    $hosts=DAO::getAll("models\Host","idUser=".$user->getId());     /** Récupère les données du model Host correspondant a l'Id */
+    $virtualhosts=DAO::getAll("models\Virtualhost", "idServer!=".$user->getId());     /** Récupère les données du model Virtualhost correspondant a l'idServeur */
+    $this->loadView("My/index.html",array("hosts"=>$hosts, "virtualhosts"=>$virtualhosts));       /** Envoi les differents tableaux de données dans la vue index.html du dossier My */
+
 
     }
+    else {
+        Auth::getInfoUser($this);
+        $message=$this->semantic->htmlMessage("error","Merci de vous connecter pour tester.");
+        $message->setIcon("announcement")->setError();
+        $message->setDismissable();
+        $message->addContent(Auth::getInfoUser($this,"-login"));
+        echo $message;
+        echo $this->jquery->compile($this->view);
+
+    }
+
+  }
 
 }
